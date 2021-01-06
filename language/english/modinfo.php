2c3007a64feb8b45347819e48e77fecba42f5fd5
eaouyuan <eaouyuan@gmail.com>
2021-01-06 09:22:57 +0800

    初始化

---
+++ language/english/modinfo.php
@@ -0,0 +1,5 @@
+<?php
+xoops_loadLanguage('modinfo_common', 'tadtools');
+
+define("_MI_beck_iscore_ADMENU1", "Teacher management");
+define("_MI_beck_iscore_ADMENU1_DESC", "Admin Teacher management");
---
+++ language/tchinese_utf8/modinfo.php
@@ -0,0 +1,11 @@
+<?php
+xoops_loadLanguage('modinfo_common', 'tadtools');
+
+define("_MI_beck_iscore_NAME", "分校成績輸入");
+define("_MI_beck_iscore_DESCRIPTION", "模組說明");
+
+define("_MI_beck_iscore_ADMENU1", "教師管理");
+define("_MI_beck_iscore_ADMENU1_DESC", "後台教師管理");
+
+define("_MI_beck_iscore_ADMENU2", "學程管理");
+define("_MI_beck_iscore_ADMENU2_DESC", "學程管理");
