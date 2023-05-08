3ecd0f7850496d1e204ee5509a3d2182dd615d72
eaouyuan <eaouyuan@gmail.com>
2023-05-08 16:13:39 +0800

    token timeout set 360 to 1800

--- index.php
+++ index.php
@@ -301,7 +301,7 @@ default:
         }
         $xoopsTpl->assign('op', $op);
 
-        $token =new XoopsFormHiddenToken('XOOPS_TOKEN',360);
+        $token =new XoopsFormHiddenToken('XOOPS_TOKEN',1800);
         $xoopsTpl->assign('XOOPS_TOKEN' , $token->render());
 
     }
@@ -649,7 +649,7 @@ default:
         }
         $xoopsTpl->assign('op', $op);
 
-        $token =new XoopsFormHiddenToken('XOOPS_TOKEN',360);
+        $token =new XoopsFormHiddenToken('XOOPS_TOKEN',1800);
         $xoopsTpl->assign('XOOPS_TOKEN' , $token->render());
 
     }
